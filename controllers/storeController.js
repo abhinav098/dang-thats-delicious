fb29144c25ee34fa5de4d3120395f870e499c551
Abhinav Garg <abhinav.g@kiprosh.com>
2018-10-11 11:20:47 +0530

    introducing middlewares

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,9 @@
+exports.myMiddleware = (req, res, next) =>{
+  req.name = "Abhinav";
+  next();
+};
+
 exports.homepage = (req, res) => {
+  console.log(req.name);
   res.render('index', {title: 'Dang'});
 };
