fd471df9e29642d0f42cb6fe2fa799d379951a2f
Abhinav Garg <abhinav.g@kiprosh.com>
2018-10-11 12:10:32 +0530

    add model

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,9 +1,3 @@
-exports.myMiddleware = (req, res, next) =>{
-  req.name = "Abhinav";
-  next();
-};
-
 exports.homepage = (req, res) => {
-  console.log(req.name);
   res.render('index', {title: 'Dang'});
 };
